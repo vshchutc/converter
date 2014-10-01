e9067cffb81c87d25d70eef5a7e7e8c6a2b0c44e
vshchutc <victoria.schutska@gmail.com>
2014-10-01 17:28:15 +0300

    without hardcode

--- javascript.js
+++ javascript.js
@@ -1,120 +1,65 @@
 $(document).ready(function() {
+	
 	$( "#go" ).click(function(e) {
 		e.preventDefault();
 		var numbPre=$("#number").val();
 		var totalNumb=parseInt(numbPre);
-		if(isNaN(totalNumb)||totalNumb<=0||totalNumb>20000){
+		if (isNaN(totalNumb)||totalNumb<=0||totalNumb>1000) {
 			alert("Enter valid integer: Number has to be positive, less than 10 000, also you can't convert a text");
 			$("#number").css("color", "#BD5C5C");
-			$("#number").change(function(){
-			$("#number").css("color", "#000")})
-			;}
-		else {
+			} else {
 			var roman="";
-			var numbWithoutThousands=0;
-			var fivehundr=0;
-			var numbWithoutHundreds=0;
-		if(totalNumb>=1000){
-			var thousands=(totalNumb-totalNumb%1000)/1000;
-			for(i=0;i<thousands;i++){
-				roman+="M"
+			
+			var unitsArr=["I","V","X"];
+			var tensArr=["X","L","C"];
+			var hundredsArr=["C","D","M"];
+			function count(arrayName,quantity,romResult ) {
+				switch(quantity){
+					case 0:
+						romResult="";
+						break;
+					case 1:
+						romResult=arrayName[0];
+						break;
+					case 2:
+						romResult=arrayName[0]+arrayName[0];
+						break;
+					case 3:
+						romResult=arrayName[0]+arrayName[0]+arrayName[0];
+						break;
+					case 4:
+						romResult=arrayName[0]+arrayName[1];
+						break;
+					case 5:
+						romResult=arrayName[1];
+						break;
+					case 6:
+						romResult=arrayName[1]+arrayName[0];
+						break;
+					case 7:
+						romResult=arrayName[1]+arrayName[0]+arrayName[0];
+					case 8:
+						romResult=arrayName[1]+arrayName[0]+arrayName[0]+arrayName[0];
+						break;
+					case 9:
+						romResult=arrayName[0]+arrayName[2];
+						break;
 				};
-			numbWithoutThousands+=totalNumb%1000;
-		} else {
-		numbWithoutThousands=totalNumb
-		};
-		var hundreds=(numbWithoutThousands-numbWithoutThousands%100)/100;
-		switch(hundreds){
-			case 1:
-				roman+="C";
-				break;
-			case 2:
-				roman+="CC";
-				break;
-			case 3:
-				roman+="CCC";
-				break;
-			case 4:
-				roman+="CD";
-				break;
-			case 5:
-				roman+="D";
-				break;
-			case 6:
-				roman+="DC";
-				break;
-			case 7:
-				roman+="DCC";
-				break;
-			case 8:
-				roman+="DCCC";
-				break;
-			case 9:
-				roman+="CM"
-				break;
-		};
-		numbWithoutHundreds+=numbWithoutThousands%100;
-		var tens=(numbWithoutHundreds-numbWithoutHundreds%10)/10;
-		switch(tens){
-			case 1:
-				roman+="X";
-				break;
-			case 2:
-				roman+="XX";
-				break;
-			case 3:
-				roman+="XXX";
-				break;
-			case 4:
-				roman+="XL";
-				break;
-			case 5:
-				roman+="L";
-				break;
-			case 6:
-				roman+="LX";
-				break;
-			case 7:
-				roman+="LXX";
-				break;
-			case 8:
-				roman+="LXXX";
-				break;
-			case 9:
-				roman+="XC"
-				break;
-		};
-		var units=numbWithoutHundreds%10;
-		switch(units){
-			case 1:
-				roman+="I";
-				break;
-			case 2:
-				roman+="II";
-				break;
-			case 3:
-				roman+="III";
-				break;
-			case 4:
-				roman+="IV";
-				break;
-			case 5:
-				roman+="V";
-				break;
-			case 6:
-				roman+="VI";
-				break;
-			case 7:
-				roman+="VII";
-				break;
-			case 8:
-				roman+="VIII";
-				break;
-			case 9:
-				roman+="IX"
-				break;
-		};
-	};
-	$("#result").empty();
-	$('#result').append(roman);});
+				return romResult;
+			};
+			
+			var withoutHundreds=totalNumb%100;
+			var withoutTens=withoutHundreds%10;
+			var hundredsQuantity=(totalNumb-withoutHundreds)/100;
+			var tensQuantity=(withoutHundreds-withoutTens)/10;
+			var unitsQuantity=withoutTens;
+			roman+=count(hundredsArr,hundredsQuantity)+count(tensArr,tensQuantity)+count(unitsArr,unitsQuantity);
+			}
+		$("#result").empty();
+		$('#result').append(roman);
+	});
+	
+
+	
 });
+		
